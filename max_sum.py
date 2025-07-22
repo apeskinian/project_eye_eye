04c181f383568a111567e04bea08ad722f6702fd
Matthew Aitken <matt@itken.net>
2025-07-22 09:49:20 +0100

    Add steps needed

---
+++ max_sum.py
@@ -0,0 +1,39 @@
+"""
+- Matthew Aitken
+- 2i Testing Coding Challenge
+- 22nd July 2025
+
+- Task:
+    Given an array of strings, each made up of a combination of letters and
+    digits, write the functionality to find and return the largest sum of
+    digits within a string.
+
+    Considering a variety of different input arrays, state any assumptions or
+    design choices you had to make.
+
+    Notes:
+    - Each digit should be considered its own 1-digit number i.e. in the third
+    string below 36 is evaluated as 3 and 6 separately.
+    - The input arrays can vary in length; however, they will not be larger
+    than 10 strings.
+    - Strings can vary in length; however, they will not be larger than 12
+    characters.
+
+    Example:
+    Input - [ "dh7js4jf", "or2rjvn2w", "h1n36mfl", "a7e6fw" ]
+    Sums - 11, 4, 10, 13
+    Output - 13
+"""
+
+
+def main():
+    """
+    Main entry point of program.
+    """
+    # send array to method that will filter out numbers and return
+    # sum the numbers for each array
+    # deduce the highest and return that
+
+
+if __name__ == "__main__":
+    main()
